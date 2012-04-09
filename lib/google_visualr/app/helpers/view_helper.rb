e52e96c8bbfcc2b22dd0b77d67d2da6a34c72b4c
Ahmer Arif <ahmer.arif@gmail.com>
2012-04-09 14:01:27 +0500

    Added some helpers for my own personal convenience

--- lib/google_visualr/app/helpers/view_helper.rb
+++ lib/google_visualr/app/helpers/view_helper.rb
@@ -8,10 +8,25 @@ module GoogleVisualr
 
       included do
         helper_method "render_chart"
+		helper_method "render_chart_with_events"
+        helper_method "render_chart_without_script"
+        helper_method "render_chart_with_callback"
       end
 
       def render_chart(chart, dom)
         chart.to_js(dom).html_safe
+      end
+	  
+	  def render_chart_with_events(chart, dom)
+        chart.to_js_with_events(dom).html_safe
+      end
+	  
+	  def render_chart_without_script(chart, dom)
+        chart.to_js_without_script(dom).html_safe
+      end
+
+	  def render_chart_with_callback(chart, dom)
+        chart.to_js_add_callback(dom).html_safe
       end
 
     end
--- lib/google_visualr/base_chart.rb
+++ lib/google_visualr/base_chart.rb
@@ -34,6 +34,41 @@ module GoogleVisualr
 
       js
     end
+	
+	def to_js_with_events(element_id)
+      js  = "\n<script type='text/javascript'>"
+      js << "\n  google.load('visualization','1', {packages: ['#{package_name}'], callback: function() {"
+      js << "\n    #{@data_table.to_js}"
+      js << "\n    var chart = new google.visualization.#{class_name}(document.getElementById('#{element_id}'));"
+      js << "\n    chart.draw(data_table, #{js_parameters(@options)});"
+	  js << "\n    add_events(data_table, chart);"
+      js << "\n  }});"
+      js << "\n</script>"
+
+      js
+    end
+	
+	def to_js_without_script(element_id)
+      js = "\n  google.load('visualization','1', {packages: ['#{package_name}'], callback: function() {"
+      js << "\n    #{@data_table.to_js}"
+      js << "\n    var chart = new google.visualization.#{class_name}(document.getElementById('#{element_id}'));"
+      js << "\n    chart.draw(data_table, #{js_parameters(@options)});"
+      js << "\n  }});"
+
+      js
+    end
+	
+	def to_js_add_callback(element_id)
+      js = "\n  google.load('visualization','1', {packages: ['#{package_name}'], callback: function() {"
+      js << "\n    #{@data_table.to_js}"
+      js << "\n    var chart = new google.visualization.#{class_name}(document.getElementById('#{element_id}'));"
+      js << "\n    chart.draw(data_table, #{js_parameters(@options)});"
+	  js << "\n	   google.visualization.events.addListener(chart, 'ready', myReadyHandler);"
+      js << "\n  }});"
+
+      js
+    end
+	
 
   end
 
